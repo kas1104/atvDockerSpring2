fa17d34e0e19c1e25bb3769d2ea2c833fcaf8aff
kas1104 <115120780+kas1104@users.noreply.github.com>
2026-05-07 16:21:23 -0300

    Tarefa Completa

---
+++ atvDockerSpring2/src/main/java/com/aula/dockerSpring/atvDockerSpringApplication.java
@@ -0,0 +1,13 @@
+package com.aula.dockerSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class atvDockerSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(atvDockerSpringApplication.class, args);
+	}
+
+}
